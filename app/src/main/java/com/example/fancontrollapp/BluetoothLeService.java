11f76ab570878a4fd821bb0e375623cd7c6188b0
helena615 <ning980615@gmail.com>
2023-05-17 21:08:04 +0200

    fix bug read characteristic

--- app/src/main/java/com/example/fancontrollapp/BluetoothLeService.java
+++ app/src/main/java/com/example/fancontrollapp/BluetoothLeService.java
@@ -1,7 +1,5 @@
 package com.example.fancontrollapp;
 
-import static android.content.ContentValues.TAG;
-
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -13,6 +11,7 @@ import android.bluetooth.BluetoothProfile;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.wifi.aware.Characteristics;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
@@ -20,12 +19,15 @@ import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class BluetoothLeService extends Service {
     public BluetoothLeService() {
     }
+
     private static final String uuid = "00000001-0000-0000-FDFD-FDFDFDFDFDFD";
     private Binder binder = new LocalBinder();
     private BluetoothGatt bluetoothGatt;
@@ -47,6 +49,8 @@ public class BluetoothLeService extends Service {
     }
 
     public boolean connect(final String address) {
+        Log.d(TAG, "enter service connect");
+
         if (bluetoothAdapter == null || address == null) {
             Log.w(TAG, "BluetoothAdapter not initialized or unspecified address.");
             return false;
@@ -64,15 +68,21 @@ public class BluetoothLeService extends Service {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            // return TODO;
+
             Toast.makeText(this, "No BLUETOOTH_CONNECT permission!", Toast.LENGTH_SHORT).show();
             Log.d(TAG, "No BLUETOOTH_CONNECT permission!");
         }
+
+        // connect to the GATT server on the device
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+
+            Toast.makeText(this, "No BLUETOOTH_CONNECT permission!", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "No BLUETOOTH_CONNECT permission!");
+        }
+
+
         bluetoothGatt = device.connectGatt(this, false, bluetoothGattCallback);
 
 
@@ -91,12 +101,7 @@ public class BluetoothLeService extends Service {
                 if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
                     //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
+
                 }
                 bluetoothGatt.discoverServices();
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
@@ -110,8 +115,29 @@ public class BluetoothLeService extends Service {
                 // TODO: broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
                 Log.d(TAG, "ACTION_GATT_SERVICES_DISCOVERED");
 
-                BluetoothGattCharacteristic chari = bluetoothGatt.getServices().get(0).getCharacteristic(UUID.fromString(uuid));
-                readCharacteristic(chari);
+                UUID uid = bluetoothGatt.getServices().get(0).getUuid();
+                Log.d(TAG, String.valueOf(uid.toString().equals(uuid)));
+                List<BluetoothGattService> services = bluetoothGatt.getServices();
+                // List<Characteristics> chars = new ArrayList<>();
+                // UUID newuid = UUID.fromString("10000001-0000-0000-fdfd-fdfdfdfdfdfd");
+                // BluetoothGattCharacteristic newchar = services.get(0).getCharacteristic(newuid);
+                // readCharacteristic(newchar);
+
+                for (int i = 0; i < services.size(); i++) {
+                    // chars.add(services.get(i).getCharacteristics());
+
+                    List<BluetoothGattCharacteristic> chars = services.get(i).getCharacteristics();
+                    for (int j = 0; j < chars.size(); j++) {
+                        readCharacteristic(chars.get(j));
+                    }
+                }
+
+
+                // BluetoothGattCharacteristic chari = bluetoothGatt.getServices().get(0).getCharacteristic(UUID.fromString(uuid));
+                // BluetoothGattCharacteristic chari[] = bluetoothGatt.getServices().get(0).getCharacteristics().toArray(new BluetoothGattCharacteristic[0]);
+                // Log.d(TAG, "gatt service discovered func :"+ chari.toString());
+                // readCharacteristic(chari[0]);
+
             } else {
                 Log.w(TAG, "onServicesDiscovered received: " + status);
             }
@@ -125,12 +151,31 @@ public class BluetoothLeService extends Service {
             if (status == BluetoothGatt.GATT_SUCCESS) {
                 // TODO: broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
                 Log.d(TAG, "read chari success");
-                Log.d(TAG, "CHARI: "+ characteristic.toString());
+                Log.d(TAG, "CHARI VALUE: " + new String(characteristic.getValue(), StandardCharsets.UTF_8));
+                if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                gatt.setCharacteristicNotification(characteristic, true);
+                // byte[] byte_on = "1111".getBytes();
+                byte[] byte_on = {11,11};
+                characteristic.setValue(byte_on);
+                Log.d(TAG, "byte on: " + byte_on);
+            }
+            else{
+                Log.d(TAG, "GATT not success on read.");
             }
         }
     };
 
     public void readCharacteristic(BluetoothGattCharacteristic characteristic) {
+        Log.d(TAG, "Func called: read characteristic func started.");
         if (bluetoothGatt == null) {
             Log.w(TAG, "BluetoothGatt not initialized");
             return;
@@ -141,6 +186,8 @@ public class BluetoothLeService extends Service {
             return;
         }
         bluetoothGatt.readCharacteristic(characteristic);
+
+
     }
 
     public List<BluetoothGattService> getSupportedGattServices() {
--- app/src/main/java/com/example/fancontrollapp/MainActivity.java
+++ app/src/main/java/com/example/fancontrollapp/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.fancontrollapp;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothClass;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
@@ -21,6 +24,7 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
 import android.os.ParcelUuid;
 import android.util.Log;
@@ -34,11 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
     public BroadcastReceiver broadcastReceiver;
     private static final String uuid = "00000001-0000-0000-FDFD-FDFDFDFDFDFD";
-
+    // private static final String uuid = "00000002-0000-0000-FDFD-FDFDFDFDFDFD";
     private LeDeviceListAdapter leDeviceListAdapter;
     private TextView textViewConn;
     private Context context;
     private BluetoothLeService bluetoothService;
+    private BluetoothLeScanner bluetoothLeScanner;
+    private ScanSettings scanSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +57,18 @@ public class MainActivity extends AppCompatActivity {
         Button connButton = findViewById(R.id.button_conn);
         textViewConn = findViewById(R.id.textView_conn);
 
+
+        bluetoothLeScanner = ((BluetoothManager) getSystemService(BLUETOOTH_SERVICE))
+                .getAdapter().getBluetoothLeScanner();
+
         connButton.setOnClickListener(view -> {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    findDevice();
+                    // findDevice();
+                    scanLeDevice();
                 }
-            });
+            }).start();
         });
         broadcastReceiver = new BroadcastReceiver() {
             @Override
@@ -68,45 +79,147 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // public void ActivityCompat.OnRequestPermissionsResultCallback(){
 
+    // }
     // Device scan callback.
     private ScanCallback leScanCallback =
+
             new ScanCallback() {
+
+
                 @Override
+
                 public void onScanResult(int callbackType, ScanResult result) {
                     super.onScanResult(callbackType, result);
-                    leDeviceListAdapter.addDevice(result.getDevice());
-                    leDeviceListAdapter.notifyDataSetChanged();
-                    textViewConn.setText("Found Device.");
-                    Toast.makeText(context, "Found Device!", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "Found Device!");
-                    startLeService();
+                    Log.d(TAG, "enter scan call back");
+
+
+                    if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    // Toast.makeText(context, "Found Device!", Toast.LENGTH_SHORT).show();
+                    if(result.getDevice().toString().equals("F8:20:74:F7:2B:82")){
+                        Log.d(TAG, "Found Device!");
+                        textViewConn.setText("Found Device.");
+                        bluetoothLeScanner.stopScan(leScanCallback);
+                        leDeviceListAdapter.addDevice(result.getDevice());
+                        leDeviceListAdapter.notifyDataSetChanged();
+                        startLeService();
+                    }
+
+
                 }
+
+
+
+                @Override
+                public void onScanFailed(int errorCode) {
+                    super.onScanFailed(errorCode);
+                    Log.d(TAG, "scann failed." + errorCode);
+                }
+
             };
 
-    public void findDevice() {
-        BluetoothLeScanner bluetoothLeScanner = ((BluetoothManager) getSystemService(BLUETOOTH_SERVICE))
-                .getAdapter().getBluetoothLeScanner();
+
+
+
+    private boolean scanning;
+    private Handler handler = new Handler();
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+    private static final int REQUEST_COARSE_LOCATION = 1;
+    private static final int REQUEST_FINE_LOCATION = 2;
+    private static final int REQUEST_BLUETOOTH_CONNECT = 3;
+
+
+    private ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // Permission is granted. Continue the action or workflow in your
+                    // app.
+                } else {
+                    // Explain to the user that the feature is unavailable because the
+                    // feature requires a permission that the user has denied. At the
+                    // same time, respect the user's decision. Don't link to system
+                    // settings in an effort to convince the user to change their
+                    // decision.
+                }
+            });
+    private void scanLeDevice() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+
+            Log.d(TAG, "No ACCESS_FINE_LOCATION permission!");
+            // requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+
+            Log.d(TAG, "No ACCESS_FINE_LOCATION permission!");
+            // requestPermissions(new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            requestPermissionLauncher.launch(android.Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            Log.d(TAG, "No ACCESS_FINE_LOCATION permission!");
+            // requestPermissions(new String[]{android.Manifest.permission.BLUETOOTH_CONNECT}, 1);
+            requestPermissionLauncher.launch(android.Manifest.permission.BLUETOOTH_CONNECT);
+
+        }
         ArrayList<ScanFilter> scanFilters = new ArrayList<ScanFilter>();
         ParcelUuid parcelUuid = ParcelUuid.fromString(uuid);
         scanFilters.add(new ScanFilter.Builder().setServiceUuid(parcelUuid).build());
+        scanSettings = new ScanSettings.Builder().build();
 
-        ScanSettings scanSettings = new ScanSettings.Builder().build();
-        // scanLeDevice(scanFilters);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            // return ;
-            Toast.makeText(this, "No bluetooth_scan permission!", Toast.LENGTH_SHORT).show();
-            Log.d(TAG, "No bluetooth_scan permission!");
+        if (!scanning) {
+            // Stops scanning after a predefined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    scanning = false;
+                    if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        Log.d(TAG, "no permission");
+                        return;
+                    }
+                    bluetoothLeScanner.stopScan( leScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            scanning = true;
+            Log.d(TAG, "start scan");
+            bluetoothLeScanner.startScan(leScanCallback);
+            // bluetoothLeScanner.startScan(scanFilters, scanSettings, leScanCallback);
+        } else {
+            scanning = false;
+            Log.d(TAG, "start scan");
+            bluetoothLeScanner.stopScan(leScanCallback);
+
         }
-        bluetoothLeScanner.startScan(scanFilters, scanSettings, leScanCallback);
     }
+
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
@@ -118,6 +231,7 @@ public class MainActivity extends AppCompatActivity {
                     Log.e(TAG, "Unable to initialize Bluetooth");
                     finish();
                 }
+
                 String address = leDeviceListAdapter.getDevice(0).getAddress();
                 bluetoothService.connect(address);
             }
@@ -129,6 +243,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
     private void startLeService() {
+        Log.d(TAG, "start le service func start");
         Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
         bindService(gattServiceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
     }
